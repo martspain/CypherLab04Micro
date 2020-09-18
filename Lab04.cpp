1a2925d9188fb7bd11ad4490e18f7b19a8b29d62
Martín España <martineduardoer00@gmail.com>
2020-09-17 18:54:40 -0600

    Se pulió la estética del código y se cambio el texto en FUENTE.txt al de prueba

--- Lab04.cpp
+++ Lab04.cpp
@@ -39,11 +39,12 @@ using namespace std;
 /*******************
 Variables globales
 *******************/
-string const fileName = "FUENTE.txt";	//Nombre del archivo fuente
-string const keyWord = "Gu4T"; 			//Palabra clave de cifrado
-string cypherDone = "";					//Variable que almacenara el texto cifrado
-int const bufferLength = 8;				//Espacio del buffer (cuantas letras se cifran seguidas)
-int const threadCount = 4;				//Numero de threads utilizados
+string const fileName = "FUENTE.txt";	//Nombre del archivo fuente 								(editable)
+string const keyWord = "Gu4T"; 			//Palabra clave de cifrado									(editable)
+string cypherDone = "";					//Variable que almacenara el texto cifrado					(no editable)
+int const bufferLength = 8;				//Espacio del buffer (cuantas letras se cifran seguidas)	(¡¡¡peligroso editar!!!)
+int const threadCount = 4;				//Numero de threads utilizados								(editable)
+
 /*----- Variables globales compartidas -----*/
 int counter = 0;
 pthread_cond_t cola_llena, cola_vacia; 
@@ -78,8 +79,11 @@ string readFile(){
 }
 
 
-//Subrutina para cifrar un string de 8 caracteres (Aquí se utiliza el XOR con cada pthread)
+/*****************************************************************************************
+Subrutina para cifrar un string de 8 caracteres (Aquí se utiliza el XOR con cada pthread)
+*****************************************************************************************/
 void *cypherText(void *argument){
+	
 	//Se hace la conversion del argumento a un string y se guarda en oldString
 	string &oldString = *(static_cast<string*>(argument));
 	string newString = "";
@@ -93,22 +97,20 @@ void *cypherText(void *argument){
 
 		pthread_cond_broadcast(&cola_vacia);
 		pthread_cond_wait(&cola_llena, &mutex_forvar);
-		
 	}
 	else
 	{
 		counter++;
-	
-		
-		//while(pthread_cond_wait(&cola_llena, &mutex_forvar) != 0);
 	}
 
 	
-	//*******************
-	//Se hace el cifrado
-	//*******************
+	//***********************************************
+	//Se hace el cifrado (ABCD EFGH) --> (efgh abcd)*
+	//***********************************************
+	//Ciclos for que incluyen los ultimos cuatro caracteres del string 
+	
+	//Ronda 1
 	int a=0;
-	//For que incluye los ultimos cuatro caracteres del string (Ronda 1)
 	for(int i=bufferLength-4;i<bufferLength;i++){
 		newString += (oldString[i]^keyWord[a]);
 		a++;
@@ -116,6 +118,7 @@ void *cypherText(void *argument){
 	for(int j=0;j<bufferLength-4;j++){
 		newString += oldString[j];
 	}
+	
 	//Ronda 2
 	oldString="";
 	a=0;
@@ -125,6 +128,7 @@ void *cypherText(void *argument){
 	for(int j=0;j<bufferLength-4;j++){
 		oldString += newString[j];
 	}
+	
 	//Ronda 3
 	newString="";
 	a=0;
@@ -134,6 +138,7 @@ void *cypherText(void *argument){
 	for(int j=0;j<bufferLength-4;j++){
 		newString += oldString[j];
 	}
+	
 	//Ronda 4
 	oldString="";
 	a=0;
@@ -143,8 +148,6 @@ void *cypherText(void *argument){
 	for(int j=0;j<bufferLength-4;j++){
 		oldString += newString[j];
 	}
-
-	//SPFIAAAAAAA
 	
 	//Se añade el string cifrado al texto global cifrado
 	cypherDone += oldString;
@@ -158,17 +161,22 @@ void *cypherText(void *argument){
 //Main
 int main(){
 	
+	//Inicialización de variables
 	int rc;
 	int option;
 	string text;
-	text = readFile();
 	bool active = true;
+	
+	//Se lee el archivo
+	text = readFile();
+	
+	//Se inicializan las variables de pthread, de mutex y de cond
 	pthread_t threadID;
-
 	pthread_mutex_init(&mutex_forvar, NULL);
 	pthread_cond_init(&cola_llena, NULL); 
 	pthread_cond_init(&cola_vacia, NULL);
 	
+	//Se muestra el banner del programa.
 	cout<<"Universidad del Valle de Guatemala"<<endl;
 	cout<<"Programación de Microprocesadores"<<endl;
 	cout<<"Laboratorio 4: Cifrado de textos"<<endl;
@@ -176,20 +184,21 @@ int main(){
 	cout<<"Diego Ruiz	Carné: 18761 \nMartín España	Carné: 19258"<<endl;
 	
 	while(active){
+		//Se muestra el menu y se solicita una opción.
 		cout<<"\nMenu de Opciones \n¿Qué desea hacer? \n1. Cifrar texto. \n2. Salir."<<endl;
 		cin>>option;
 		
-		//Si se elige cifrar el texto
+		//Si se elige cifrar el texto...
 		if(option == 1){
 			if(text==""){
 				cout<<"El archivo "<<fileName<<" esta vacio."<<endl;
 			}
 			else{
-
-				string temporary = "";
-				int letterGroups;
-				int a = 0; //contador de las 8 letras
-				int rept;
+				cypherDone = "";			//Se vacía el texto cifrado en caso deseen repetir el proceso
+				string temporary = "";		//Variable que sirve para separar el texto
+				int letterGroups;			//Cantidad de grupos de 8 caracteres que se formaran a partir del texto
+				int a = 0; 					//Contador de las 8 letras
+				int rept;					//Contador de las repeticiones (1-8)
 				
 				//Se determina cuantos grupos de caracteres se crearan a partir del tamaño del buffer definido
 				if(((text.length())%bufferLength)>0){
@@ -199,18 +208,22 @@ int main(){
 					letterGroups = (int)(text.length()/bufferLength);
 				}
 				
+				//Se crea el array de la longitud de cantidad de grupos de 8 caracteres
 				string collection [letterGroups];
 				
 				//Se recorre el texto y se separa en sus respectivos grupos, los cuales se almacenan en collection
 				for(int i=0;i<letterGroups;i++){
 					rept = 0;
 					
+					//Si se estan comparando las últimas letras esto asegura que no va a sobrepasar el limite del texto (out of bounds prevention)
 					if((a+bufferLength-1)>(text.length()-1)){
 						while(rept < bufferLength && (a+rept)<(text.length()-1)){
 							temporary += text[a+rept];
 							rept++;
 						}
 					}
+					
+					//Si se comparan las letras de cualquier otra parte del texto se pueden separar normalmente
 					else{
 						while(rept < bufferLength){
 							temporary += text[a+rept];
@@ -227,15 +240,16 @@ int main(){
 				IMPORTANTE: EN ESTE PUNTO, TODOS LOS GRUPOS DE 8 CARACTERES ESTAN EN EL ARRAY "COLLECTION"
 				*****************************************************************************************/
 				
-				//Se crea un vector con los grupos de caracteres (STACK FIFO)
+				//Se crea un vector con los grupos de caracteres (STACK)
 				vector<string> stack;
 				for(int i=0;i<letterGroups;i++){
 					stack.push_back(collection[i]);
 				}
 				
-				/******************************************************
-				FUNCIONA PERO NO LO MUESTRA EN ORDEN (HACEN FALTA MUTEX)
-				*******************************************************/
+				/***********************************************************************************************
+				Se asigna un grupo de 8 caracteres a cada thread hasta que se agoten los grupos de 8 caracteres.
+				Para ello se utiliza la estructura de datos "stack" a través de un vector.
+				***********************************************************************************************/
 				while(stack.size()>0){
 					for(int j=0;j<threadCount && stack.size()>0;j++){
 						
@@ -279,10 +293,14 @@ int main(){
 			//Si sobrevive hasta aca, la operación fue exitosa
 			cout<<"El texto cifrado se ha escrito exitosamente."<<endl;
 		}
+		
+		//Si se elige salir del programa...
 		else if(option == 2){
 			cout<<"Gracias por utilizar el programa. ¡Vuelva pronto!"<<endl;
 			active = false;
 		}
+		
+		//Si ingresan una opción no valida...
 		else{
 			cout<<"Por favor, ingrese una opción válida"<<endl;
 		}
